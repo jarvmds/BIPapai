f3418b3640ab76eafcc3e1c03bedcb96bc22316d
Jéssica Raya <95510461+jarvmds@users.noreply.github.com>
2024-11-04 21:51:30 -0300

    test2

---
+++ README.md
@@ -0,0 +1,121 @@
+# ✨ Awesome Panel Examples
+
+A repository of awesome [Panel](https://panel.holoviz.org/) data app examples.
+
+The source files are available in the [src](/src) folder.
+
+The apps are converted to webassembly and released to
+[awesome-panel.github.io/examples/](https://awesome-panel.github.io/examples/) as simple as
+
+1. `$ panel convert app.py` (1 second)
+2. `$ git push` (1 second)
+
+This is **THE FASTEST WAY TO DEPLOY DATA APPS** in Python.
+
+The apps are **very performant** when loaded because there is no latency for communication between
+server and client.
+
+![awesome-panel-examples-tour.gif](assets/gifs/awesome-panel-examples-tour.gif)
+
+BUT the apps **take time to load** as they need to download a Python runtime and additional packages
+(50MB per app).
+
+This is **THE FUTURE OF DATAVIZ** in Python.
+
+If you like [Panel](https://panel.holoviz.org/) and [Awesome Panel](https://awesome-panel.org) please support the projects by giving them a ⭐ on Github
+
+[⭐ holoviz/panel](https://github.com/holoviz/panel) [⭐ awesome-panel/awesome-panel](https://github.com/awesome-panel/awesome-panel) [⭐ awesome-panel/examples](https://github.com/awesome-panel/examples)
+
+Thanks 👍
+
+## 📙 How to
+
+### ⚙️ Install the repository
+
+```bash
+git clone https://github.com/awesome-panel/examples.git
+```
+
+Create and activate a virtual environment, c.f.
+[Python Virtual Environments: A Primer](https://realpython.com/python-virtual-environments-a-primer/).
+
+Install the `requirements.txt` file
+
+```bash
+pip install -r requirements.txt -U
+```
+
+### 💻 Serve a server app locally
+
+You can serve for example the [hello-world](src/hello-world/app.py) application on your Panel server via
+
+```python
+panel serve src/hello-world/app.py --autoreload
+```
+
+It will be available at [http://localhost:5006/app](http://localhost:5006/app).
+
+![Panel Hello World App](assets/images/hello-world.png)
+
+### 🏗️ Convert an app to webassembly
+
+You can [panel convert](https://panel.holoviz.org/user_guide/Running_in_Webassembly.html) for
+example the [hello-world](src/hello-world/app.py) application to webassembly via
+
+```python
+panel convert src/hello-world/app.py --to pyodide-worker --out docs/hello-world
+```
+
+If you want to avoid repeating the big download over and over again, you can even convert to a
+[progressive web app](https://en.wikipedia.org/wiki/Progressive_web_app) that can be installed on
+your laptop! It is as simple as adding the `--pwa` and `--title` flags.
+
+Check out the [Panel WebAssembly Guide](https://panel.holoviz.org/user_guide/Running_in_Webassembly.html)
+for more details.
+
+### 💻 Serve a webassembly app locally
+
+```bash
+python3 -m http.server
+```
+
+The app is now available at [http://localhost:8000/docs/hello-world/app.html](http://localhost:8000/docs/hello-world/app.html)
+
+### ⚙️ Configure the repository for Github Pages
+
+You can learn how to configure Github Pages in general via the [Quickstart for Github Pages](https://docs.github.com/en/pages/quickstart).
+
+The Github Pages of `awesome-panel/examples` is configured as described below
+
+1. Add a `.nojekyll` file push all changes.
+2. Navigate to the *Settings* page
+3. Navigate to the *Pages* page
+4. Configure your Pages Settings as shown below
+
+![Github Pages Settings](assets/images/gh-pages-settings.png)
+
+### 🔥 Release to Github Pages
+
+Git `add`, `commit`, `push` and *merge* your PR as you would normally do.
+
+The applications are now available at [awesome-panel.github.io/examples/](awesome-panel.github.io/examples/) or similar.
+
+## Development
+
+The below instructions are preliminary and for the project contributors
+
+### Build the project
+
+```bash
+panel convert src/hello-world/app.py --to pyodide-worker --out docs/hello-world
+panel convert src/streaming-indicators/app.py --to pyodide-worker --out docs/streaming-indicators
+panel convert src/stumpy-dashboard/app.py --to pyodide-worker --out docs/stumpy-dashboard
+panel convert src/videostream-interface/app.py --to pyodide-worker --out docs/videostream-interface --requirements src/videostream-interface/requirements.txt
+python src/index/index.py
+```
+
+```bash
+python3 -m http.server
+```
+
+[http://localhost:8000/docs/index.html](http://localhost:8000/docs/index.html)
---
+++ docs/streaming-indicators/app.js
@@ -0,0 +1,154 @@
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js");
+
+function sendPatch(patch, buffers, msg_id) {
+  self.postMessage({
+    type: 'patch',
+    patch: patch,
+    buffers: buffers
+  })
+}
+
+async function startApplication() {
+  console.log("Loading pyodide!");
+  self.postMessage({type: 'status', msg: 'Loading pyodide'})
+  self.pyodide = await loadPyodide();
+  self.pyodide.globals.set("sendPatch", sendPatch);
+  console.log("Loaded!");
+  await self.pyodide.loadPackage("micropip");
+  const env_spec = ['https://cdn.holoviz.org/panel/0.14.0/dist/wheels/bokeh-2.4.3-py3-none-any.whl', 'https://cdn.holoviz.org/panel/0.14.0/dist/wheels/panel-0.14.0-py3-none-any.whl', 'numpy', 'typing']
+  for (const pkg of env_spec) {
+    const pkg_name = pkg.split('/').slice(-1)[0].split('-')[0]
+    self.postMessage({type: 'status', msg: `Installing ${pkg_name}`})
+    await self.pyodide.runPythonAsync(`
+      import micropip
+      await micropip.install('${pkg}');
+    `);
+  }
+  console.log("Packages loaded!");
+  self.postMessage({type: 'status', msg: 'Executing code'})
+  const code = `
+  
+import asyncio
+
+from panel.io.pyodide import init_doc, write_doc
+
+init_doc()
+
+"""This example shows a streaming dashboard with Panel."""
+from typing import List, Tuple
+
+import numpy as np
+import panel as pn
+
+ACCENT = "#1f77b4"
+OK_COLOR = "#2ca02c"
+ERROR_COLOR = "#d62728"
+PERIOD = 500
+
+
+def _increment(value):
+    value = np.random.normal(1, 0.1, 1)[0] * 75
+    value = max(0, value)
+    value = min(100, value)
+    return int(value)
+
+
+def _create_callback(card):
+    def update_card():
+        card.value = _increment(card.value)
+
+    return update_card
+
+
+template = pn.template.FastGridTemplate(
+    site="Awesome Panel",
+    site_url="https://awesome-panel.org",
+    title="Streaming Indicators",
+    row_height=160,
+    accent_base_color=ACCENT,
+    header_background=ACCENT,
+    prevent_collision=True,
+    save_layout=True,
+    favicon="https://raw.githubusercontent.com/MarcSkovMadsen/awesome-panel-assets/320297ccb92773da099f6b97d267cc0433b67c23/favicon/ap-1f77b4.ico",
+)
+rstep = 1
+cstep = 2
+colors: List[Tuple[float, str]] = [(66, OK_COLOR), (100, ERROR_COLOR)]
+for row in range(0, 3, rstep):
+    for col in range(0, 12, cstep):
+        indicator = pn.indicators.Number(
+            name=f"Sensor {row, col}",
+            value=65,
+            format="{value}%",
+            colors=colors,
+            css_classes=["pn-stats-card"],
+            sizing_mode="stretch_both",
+        )
+        template.main[row, col : col + cstep] = indicator
+
+        pn.state.add_periodic_callback(_create_callback(indicator), period=PERIOD)
+
+rstep = 2
+cstep = 2
+for row in range(3, 6, rstep):
+    for col in range(0, 12, 2):
+        title = f"Sensor {row, col}"
+        colors = [(0.7, OK_COLOR), (1, ERROR_COLOR)]
+        indicator = pn.indicators.Gauge(
+            name=title,
+            value=65,
+            bounds=(0, 100),
+            colors=colors,
+            sizing_mode="stretch_both",
+            margin=-1,
+        )
+        template.main[row : row + rstep, col : col + cstep] = indicator
+
+        pn.state.add_periodic_callback(_create_callback(indicator), period=PERIOD)
+
+template.servable()
+
+
+await write_doc()
+  `
+  const [docs_json, render_items, root_ids] = await self.pyodide.runPythonAsync(code)
+  self.postMessage({
+    type: 'render',
+    docs_json: docs_json,
+    render_items: render_items,
+    root_ids: root_ids
+  });
+}
+
+self.onmessage = async (event) => {
+  const msg = event.data
+  if (msg.type === 'rendered') {
+    self.pyodide.runPythonAsync(`
+    from panel.io.state import state
+    from panel.io.pyodide import _link_docs_worker
+
+    _link_docs_worker(state.curdoc, sendPatch, setter='js')
+    `)
+  } else if (msg.type === 'patch') {
+    self.pyodide.runPythonAsync(`
+    import json
+
+    state.curdoc.apply_json_patch(json.loads('${msg.patch}'), setter='js')
+    `)
+    self.postMessage({type: 'idle'})
+  } else if (msg.type === 'location') {
+    self.pyodide.runPythonAsync(`
+    import json
+    from panel.io.state import state
+    from panel.util import edit_readonly
+    if state.location:
+        loc_data = json.loads("""${msg.location}""")
+        with edit_readonly(state.location):
+            state.location.param.update({
+                k: v for k, v in loc_data.items() if k in state.location.param
+            })
+    `)
+  }
+}
+
+startApplication()
